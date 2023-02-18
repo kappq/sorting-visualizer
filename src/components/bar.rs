9aeae0850ba7bf9ae49c972c6a2cbb8655adf05d
Kappa <f.cappetti.05@gmail.com>
2023-02-18 18:16:17 +0100

    Revert "Extract `calculate_new_height` function"
    This reverts commit 9048b43a4a160133c7e6c3411cca7bc9e2fbf650.

--- src/components/bar.rs
+++ src/components/bar.rs
@@ -2,24 +2,20 @@ use leptos::*;
 
 use crate::State;
 
-fn calculate_new_height(height: f32, max_height: f32) -> f32 {
-    height * (100.0 / max_height)
-}
-
 #[component]
 pub fn Bar(cx: Scope, height: usize, state: RwSignal<State>) -> impl IntoView {
     let bar_ref = NodeRef::<HtmlElement<Div>>::new(cx);
     create_effect(cx, move |_| {
         if let Some(element) = bar_ref.get() {
-            let new_height =
-                calculate_new_height(height as f32, state.with(|state| state.array_size) as f32);
+            let max_height = state.with(|state| state.array_size);
+            let new_height = height as f32 * (100_f32 / max_height as f32);
             let style = format!("height: {new_height}%;");
             element.prop("style", style);
         }
     });
 
-    let new_height =
-        calculate_new_height(height as f32, state.with(|state| state.array_size) as f32);
+    let max_height = state.with(|state| state.array_size);
+    let new_height = height as f32 * (100_f32 / max_height as f32);
     let style = format!("height: {new_height}%;");
     view! {
         cx,
